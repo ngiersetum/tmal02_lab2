753e5c552395a43365121534c0442e2b977eb714
niklas gierse <101125611+ngiersetum@users.noreply.github.com>
2023-09-12 14:28:32 +0200

    working on task 3

--- statistics.m
+++ statistics.m
@@ -38,7 +38,74 @@ ratio_t_w2 = acdata{index_slow, 87};
 %    Which one has the highest?
 [highest_fuel_ppx, index_high] = max(acdata{:, 116});
 [lowest_fuel_ppx, index_low] = min(acdata{:, 116});
-%% Task 3
+
+
+%% TASK 3
+
+% a) Wing loading vs MTOW
+
+mtows = table2array(wl_vs_mtow(:,"MTOW"));
+wls = table2array(wl_vs_mtow(:,"Perf_Maxwingloadkgm2"));
+
+pfit = polyfit(mtows, wls, 1);
+
+wl_trend_std = std(wls - polyval(pfit, mtows));
+
+hold on
+plot(mtows, wls, 'ro');
+plot(mtows, polyval(pfit, mtows), 'b')
+xlabel("MTOW [kg]")
+ylabel("Max Wing Loading [kg/m^2]")
+title("Wing loading over MTOW")
+
+%% b) Cruise velocity vs wing loading
+
+wls = table2array(acdata(:,"Perf_Maxwingloadkgm2"));
+cvs = table2array(acdata(:,"Perf_Cruise_LR_Speedkt"));
+names = table2array(acdata(:,"Name"));
+
+done = 0;
+i = 1;
+while done ~= 1
+    if isnan(wls(i)) || isnan(cvs(i))
+        wsize = size(wls);
+        wls = [wls(1:i-1,:) ; wls(i+1:wsize(1),:)];
+        cvs = [cvs(1:i-1,:) ; cvs(i+1:wsize(1),:)];
+        names = [names(1:i-1,:), ; names(i+1:wsize(1),:)];
+    else
+        i = i+1;
+    end
+
+    wsize = size(wls);
+    if i > wsize(1)
+        done = 1;
+    end
+end
+
+pfit = polyfit(wls, cvs, 1);
+
+wl_trend_std = std(cvs - polyval(pfit, wls));
+
+hold off
+hold on
+plot(wls, cvs, "ro", 'LineWidth', 1.5);
+plot(wls, polyval(pfit, wls), 'b')
+xlabel("Max Wing Loading [kg/m^2]")
+ylabel("Best Cruise Velocity [kts]")
+ylabel("Wind loading over Best Cruise Velocity")
+
+dx = 4;
+text(wls+dx, cvs, names, 'FontSize', 8);
+
+%% c) Relations
+
+% i total fuel consumption - pax capacity
+
+
+
+% ii fuel cons per pax mile - max range
+
+% iii fuel cons per pax mile - mtow
 
 
 %% Task 4
