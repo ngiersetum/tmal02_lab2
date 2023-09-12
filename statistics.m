8ce96ca471940701cacde2db05daefe68338593f
leomue1 <leo.muehlstrasser@tum.de>
2023-09-12 14:25:35 +0200

    Teilaufgaben a) bis d) Berechnungen

--- statistics.m
+++ statistics.m
@@ -10,20 +10,36 @@ load('data/actable.mat')
 average1 = mean(acdata{:, 4:17}, 'omitnan');
 average2 = mean(acdata{:, 20:56}, 'omitnan');
 average3 = mean(acdata{:, 58:117}, 'omitnan');
-average = [average1, average2, average3];
+average = [average1, nan, nan, average2, nan, average3];
 
 % Standard deviations
 
 Std1 = std(acdata{:, 4:17}, 'omitnan');
 Std2 = std(acdata{:, 20:56}, 'omitnan');
 Std3 = std(acdata{:, 58:117}, 'omitnan');
-Std = [Std1, Std2, Std3]
+Std = [Std1, nan, nan, Std2, nan, Std3];
 
-% a) Average wingload in kg/m^2
-%avg_wingload = average(1, 83);
+% a) What is the average wing-loading in [kg/m2]?
+avg_wingload = average(1, 83);
 
-% b) parameter with lowest standard deviation
-%[smallest_Std, position] = min(Std)
+% b) Which parameter has the lowest standard deviation?
+%    Why? Makes it sense to specify the standard deviation for this parameter?
+[smallest_Std, position] = min(Std);
+
+% c) Which is the fastest aircraft in the table, which one is the slowest?
+%    Do they have a difference in the thrust-to-weight ratio [N/N]?
+[max_velocity, index_fast] = max(acdata{:, 98});
+%formatSpec = 'The fastest aircraft is %s \n';
+%fprintf(formatSpec, acdata(56, 1))
+[min_velocity, index_slow] = min(acdata{:, 98});
+
+ratio_t_w1 = acdata{index_fast, 87};
+ratio_t_w2 = acdata{index_slow, 87};
+
+% d) Which aircraft has the lowest fuel consumption per passenger (pax) kilometre?
+%    Which one has the highest?
+[highest_fuel_ppx, index_high] = max(acdata{:, 116});
+[lowest_fuel_ppx, index_low] = min(acdata{:, 116});
 %% Task 3
 
 
